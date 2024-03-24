8f73f7a78c12c2f70cad83c342d80fab601dae58
Your Name <hrayhansyah@gmail.com>
2024-03-25 04:41:12 +0700

    10

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,16 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 
 
-declare global{
-  var prisma:PrismaClient| undefined
-}
+// declare global{
+//   var prisma:PrismaClient| undefined
+// }
 
-export const db = globalThis.prisma || new PrismaClient()
-// const prisma = new PrismaClient()
+// export const db = globalThis.prisma || new PrismaClient()
+// // const prisma = new PrismaClient()
 
-if(process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+// if(process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
 
 
-// export const db = new PrismaClient();
+export const db = new PrismaClient();
 
 
